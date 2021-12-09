31117afd5b4a9ecf96e2fbdfc10092e741f2d1b5
Yaroslav <93050088+ZeedKey@users.noreply.github.com>
2021-12-09 12:51:24 +0400

    Refactored code

---
+++ Refactored.js
@@ -0,0 +1,16 @@
+export function refactored(s, a, b) {
+let symb_arr = s.split("");
+
+  for (let i = symb_arr.length - 1; i > 0; i--) {
+    let letter = symb_arr[i];
+    if (letter === a || letter === b) return i;
+  }
+  return -1;
+}
+
+/**
+	Итерации лучше начинать с конца, 
+  так как первый элемент, который удовлетворяет условию, и будет искомым.
+
+	Рассматривать остальные элементы уже не нужно.
+**/
